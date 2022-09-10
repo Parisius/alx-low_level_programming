104a45660e87699c1f27e2c889d474f7d12b1845
Parisius <parisiushss03@gmail.com>
2022-09-09 17:11:13 -0700

    Doing advanced tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,29 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * main - print 2 digit combos
- *
- * Return: 0
+ *main - prints numbers
+ *Return: returns 0
  */
 int main(void)
 {
-	int a = '0';
-	int b = '0';
-	int c = '0';
-	int d;
-	int temp = '0';
-	for (a = '0' ; a <= '9' ; a++)
+	int a;
+	int b;
+
+	for (a = 0; a <= 99; a++)
 	{
-		for (b = '0' ; b <= '9' ; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			if (a == '9' && b == '9')
-			{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
 				break;
-			}
-			for (c = '0' ; c <= '9' ; c++)
-			{
-				if (a == c)
-				{
-					temp = b + 1;
-				}
-				else
-				{
-					temp = '0';
-				}
-				for (d = temp ; d <= '9' ; d++)
-				{
-					putchar(a);
-					putchar(b);
-					putchar(' ');
-					putchar(c);
-					putchar(d);
-					if (!(a == '9' && b == '8' && c == '9' && d == '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
