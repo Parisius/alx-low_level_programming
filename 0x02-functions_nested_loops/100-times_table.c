b344b6216ff30fa9083efbe56624a4598400417a
Parisius <parisiushss03@gmail.com>
2022-09-13 17:22:15 -0700

    Some advanced tasks2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,77 +1,57 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- *
- *
+ * times_table - print out times table of 0 to 12
+ * Return: Always 0.
  */
-void print_rec(int num)
-{
-	if (num >= 10)
-		print_rec(num / 10);
-	_putchar((num % 10) + '0');
-}
-/**
- *
- */
-void print_space(int num)
-{
-	if (num < 10)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-	}
-	if (num >= 10 && num < 100)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-	}
-	if (num > 100)
-	{
-		_putchar(',');
-		_putchar(' ');
-	}
-}
-/**
- *
- */
-void print_num(int num)
-{
-	if (num == 10)
-	{
-		_putchar('1');
-		_putchar('0');
-	}
-	else
-		print_rec(num);
-	print_space(num);
-}
-/**
- *print_times_table - displayes the times tables all the way up to a specified #
- *@n: this parameter holds the value of the given number for the times table
- */
+
 void print_times_table(int n)
 {
-	int i = 0, j = 0, oper = 0;
+	int i, j, k;
 
-	if (n <= 15 && n >= 0)
-	{
-		for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 		{
-			for (j = 0; j <= n; j++)
+			for (i = 0; i <= n; i++)
 			{
-				oper = i * j;
-				if (oper > 9)
-					print_num(oper);
-				else
+				for (j = 0; j <= n; j++)
 				{
-					_putchar(oper + '0');
-					print_space(oper);
-				}
+					k = i * j;
+					if (k <= 9)
+					{
+						if (j != 0)
+						{
+							_putchar(' ');
+							_putchar(' ');
+						}
+						_putchar(k + '0');
+					}
+					else if (j != '0')
+						{
+							_putchar(' ');
+						_putchar(k / 10 + '0');
+						_putchar(k % 10 + '0');
+						}
+					else
+					{
+						if (j != 0)
+						{
+							_putchar(k % 100 + '0');
+							_putchar(k / 10 + '0');
+						}
+					}
+
+			if (j == 9)
+			{
+				_putchar('\n');
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar(',');
+			}
+				}
+				j = 0;
+			}
 		}
-	}
 }
+
