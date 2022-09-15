ed6f4db7d0bca0c4a36ad44af356b377b70cfbea
Parisius <parisiushss03@gmail.com>
2022-09-15 06:30:44 -0700

    Trailing space removed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@ void more_numbers(void)
 {
 	int a;
 	int b;
-	
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 14; b++)
@@ -21,7 +21,7 @@ void more_numbers(void)
 			}
 			_putchar((b % 10) + '0');
 		}
-		
+
 		_putchar('\n');
 	}
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,13 +4,13 @@
  *print_square - prints a square
  *@size: the number of rows a columns of the square.
  *
- *Return: 
+ *Return:
  */
 void print_square(int size)
 {
 	int a = size;
 	int b = size;
-	
+
 	if (size <= 0)
 	{
 		_putchar ('\n');
