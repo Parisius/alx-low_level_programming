602499cdce14c8525d652f527a004f2a5b72cfe6
Parisius <parisiushss03@gmail.com>
2022-10-06 22:41:41 +0200

    Let's go

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,13 +2,10 @@
 #define MAIN_H
 
 
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 int _putchar(char c);
-char **strtow(char *str);
 
 #endif 
